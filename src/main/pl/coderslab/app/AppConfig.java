01544ad0e0a643be46d4c62c2a062268f7564dbe
GaworO <ola.gawor@gmail.com>
2017-09-08 13:57:23 +0200

    rozwiazanie

---
+++ src/main/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,29 @@
+package main.pl.coderslab.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "pl.coderslab.controller","pl.coderslab.entity" })
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+
+}
---
+++ src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ola
+  Date: 08.09.2017
+  Time: 13:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+Home Twitter
+</body>
+</html>
